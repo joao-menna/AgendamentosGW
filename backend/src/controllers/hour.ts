7dc620f42b42fb00fdd642f0d0d3f59016bbf1c9
joao-menna <joao.menna1907@gmail.com>
2024-04-14 11:13:40 -0300

    (backend) adicionado rotas para popular horas

--- backend/src/controllers/hour.ts
+++ backend/src/controllers/hour.ts
@@ -1,3 +1,118 @@
-export class HourController {
+import getUserFromToken from '../functions/getUserFromToken'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { hourTable } from '../schemas/hour'
+import getDB from '../functions/getDB'
+import { UNAUTHORIZED } from '../constants/tokens'
 
+export class HourController {
+  async getAll (req: FastifyRequest, rep: FastifyReply): Promise<FastifyReply> {
+    const { db, client } = await getDB()
+
+    try {
+      const hours = await db.select().from(hourTable)
+      await rep.send(hours)
+    } catch (err) {
+      await rep.code(500).send(err)
+    } finally {
+      await client.end()
+    }
+
+    return await rep
+  }
+
+  async insertAll (req: FastifyRequest, rep: FastifyReply): Promise<FastifyReply> {
+    const { authorization } = req.headers
+
+    let userAuth
+    try {
+      userAuth = await getUserFromToken(authorization ?? '')
+    } catch (err: any) {
+      await rep.code(401).send({
+        message: err.message
+      })
+      return await rep
+    }
+
+    const allowedTypes = ['owner', 'admin']
+    if (!allowedTypes.includes(userAuth.type)) {
+      await rep.code(401).send({
+        message: UNAUTHORIZED
+      })
+      return await rep
+    }
+
+    const hours = [
+      {
+        classNumber: 1,
+        start: '07:30',
+        finish: '08:15'
+      },
+      {
+        classNumber: 2,
+        start: '08:15',
+        finish: '09:00'
+      },
+      {
+        classNumber: 3,
+        start: '09:00',
+        finish: '09:45'
+      },
+      {
+        classNumber: 4,
+        start: '10:00',
+        finish: '10:45'
+      },
+      {
+        classNumber: 5,
+        start: '10:45',
+        finish: '11:45'
+      }
+    ]
+
+    const { db, client } = await getDB()
+
+    try {
+      await db.insert(hourTable).values(hours)
+    } catch (err) {
+    } finally {
+      await client.end()
+    }
+
+    return await rep
+  }
+
+  async deleteAll (req: FastifyRequest, rep: FastifyReply): Promise<FastifyReply> {
+    const { authorization } = req.headers
+
+    let userAuth
+    try {
+      userAuth = await getUserFromToken(authorization ?? '')
+    } catch (err: any) {
+      await rep.code(401).send({
+        message: err.message
+      })
+      return await rep
+    }
+
+    const allowedTypes = ['owner', 'admin']
+    if (!allowedTypes.includes(userAuth.type)) {
+      await rep.code(401).send({
+        message: UNAUTHORIZED
+      })
+      return await rep
+    }
+
+    const { db, client } = await getDB()
+
+    try {
+      await db.delete(hourTable)
+      await rep.send()
+    } catch (err) {
+      await rep.code(500).send(err)
+    } finally {
+      await client.end()
+    }
+
+    return await rep
+  }
 }
--- backend/src/routes/hour.ts
+++ backend/src/routes/hour.ts
@@ -1,4 +1,19 @@
+import { HourController } from '../controllers/hour'
 import { FastifyInstance } from 'fastify'
 
 export default async function hourRoutes (fastify: FastifyInstance): Promise<void> {
+  fastify.get('/api/v1/hour', async (req, rep) => {
+    const hourController = new HourController()
+    return await hourController.getAll(req, rep)
+  })
+
+  fastify.post('/api/v1/hour', async (req, rep) => {
+    const hourController = new HourController()
+    return await hourController.insertAll(req, rep)
+  })
+
+  fastify.delete('/api/v1/hour', async (req, rep) => {
+    const hourController = new HourController()
+    return await hourController.deleteAll(req, rep)
+  })
 }
