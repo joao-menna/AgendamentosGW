041003bcaf4fd8aae9458147aee9de813f6ce6d2
NomikuH8 <sofaco.checkin@gmail.com>
2024-06-27 21:46:37 -0300

    Added tokens to frontend

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,4 +1,4 @@
 // export const BASE_URL = "http://localhost:8080"
 export const BASE_URL = "https://agendamentosgw-backend.onrender.com"
-export const LOGIN_TOKEN = ""
-export const SESSION_TOKEN_KEY = "token"
+export const LOGIN_TOKEN = "undefined"
+export const SESSION_TOKEN_KEY = "undefined"
